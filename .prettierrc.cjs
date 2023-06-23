9313cd8e8d6272d8aa488c8fcab6e8d9b4d5f471
RoboXGamer <roboxgamer52@gmail.com>
2023-06-23 15:09:56 +0530

    Add Prettier Support

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  plugins: [require.resolve("prettier-plugin-astro")],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
+      },
+    },
+  ],
+};
